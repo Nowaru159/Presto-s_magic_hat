ab9de65f3c550c213c81e56f09d76c679da1d167
Nowaru <61397438+Nowaru159@users.noreply.github.com>
2023-02-07 17:20:48 -0300

    Add files via upload

---
+++ to_do_list.py
@@ -0,0 +1,12 @@
+to_do=[
+    "Do exercises",
+    "Work",
+    "Lunch",
+    "Sleep",
+    "Study",
+    "Eat",
+    "Go to college",
+    "Get home and sleep"
+]
+print(to_do[0])
+print(to_do[1:8])
