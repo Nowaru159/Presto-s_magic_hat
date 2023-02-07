ea8f69b10920607844e3822aff5ece7bfcea9744
Nowaru <61397438+Nowaru159@users.noreply.github.com>
2023-02-07 17:09:57 -0300

    Add files via upload

---
+++ music_list.py
@@ -0,0 +1,13 @@
+music_list = [
+    "Make a wish - NCT",
+    "Blackbird - Beatles",
+    "Naughty Boys - Pentagon",
+    "The Beginning - One ok rock",
+    "Closer - RM",
+    "Take on me - A-HA",
+    "Pelados em Santos - Mamonas assassinas",
+    "Killer Queen - Queen",
+    "The perfect boy - The Cure"
+]
+print("Here are some good music that I'm listening right now:\n")
+print(music_list)
