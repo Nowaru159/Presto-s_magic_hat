410c88310b915f53b237c0e9b97c8705b4b6e14a
Nowaru <61397438+Nowaru159@users.noreply.github.com>
2023-02-13 17:49:12 -0300

    Add files via upload

---
+++ Restaurants.py
@@ -0,0 +1,28 @@
+class Restaurant:
+    name = ""
+    type = ""
+    rating = 0.0
+    delivery = False
+    loyalty_card = False
+
+
+farofa_carioca = Restaurant()
+farofa_carioca.name = "Farofa Carioca"
+farofa_carioca.type = "Brazilian Lunch"
+farofa_carioca.rating = 4.8
+farofa_carioca.delivery = True
+farofa_carioca.loyalty_card = False
+
+cosejas = Restaurant()
+cosejas.name = "Cosejas"
+cosejas.type = "Juice"
+cosejas.rating = 4.5
+cosejas.delivery = False
+cosejas.loyalty_card = True
+
+china_in_box = Restaurant()
+china_in_box.name = "China in Box"
+china_in_box.type = "Chinese Restaurant"
+china_in_box.rating = 4.9
+china_in_box.delivery = True
+china_in_box.loyalty_card = True
