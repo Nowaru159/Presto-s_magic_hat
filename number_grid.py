7b8a6d320c5beeb91ed64d332101caf8bf30b538
Nowaru <61397438+Nowaru159@users.noreply.github.com>
2023-02-16 15:06:35 -0300

    Add files via upload

---
+++ number_grid.py
@@ -0,0 +1,14 @@
+num_grid = [
+    [10, 20, 30],
+    [40, 50, 60],
+    [70, 80, 90],
+    [0]
+]
+'''positions:
+00 01 02
+10 11 12
+20 21 22
+'''
+for row in num_grid:
+    for col in row:
+        print(col)
