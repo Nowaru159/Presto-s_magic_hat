e6ed4e8f6afba6b100dfeacc322663755a4d36a0
Nowaru <61397438+Nowaru159@users.noreply.github.com>
2023-12-14 14:39:04 -0300

    Add files via upload

---
+++ expenseSplit/expense.py
@@ -0,0 +1,10 @@
+def expense_explit():
+    total = float(input("What is the total ?\n"))
+
+    people = int(input("How many people are there to split?\n"))
+
+    part = (total/people)
+    
+    print(part " for each")
+
+
---
+++ expenseSplit/main.py
@@ -0,0 +1,4 @@
+import expense
+
+if __name__ == "__main__":
+    expense.expense_explit()
